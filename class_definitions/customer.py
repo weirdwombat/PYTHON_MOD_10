532ef08c877e20593770206cc6bfb9fbacd9488d
weirdwombat <omkennedy@dmacc.edu>
2020-07-06 18:40:32 -0500

    customer

---
+++ class_definitions/customer.py
@@ -0,0 +1,17 @@
+class Customer:
+    def __init__(self, lname, fname, cid):
+        self.last_name = lname
+        self.first_name = fname
+        self.customer_id = cid
+
+    def set_customer_id(self, cid):
+
+    def display(self):
+
+
+
+if __name__ == '__main__':
+    customer_1 = Customer()
+    try:
+        customer_2 = Customer()
+    except Attribute Error
